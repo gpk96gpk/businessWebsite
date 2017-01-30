e26635576378b2760bfb7b2e12424423a5fac7dc
José Rasmussen <j.l.rasmussengarcia@gmail.com>
2017-01-30 03:39:12 +0100

    Google map + contact info

---
+++ js/googlemap.js
@@ -0,0 +1,11 @@
+function initMap() {
+    var uluru = { lat: 51.917302, lng: 4.495713 };
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 15,
+        center: uluru
+    });
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+    });
+}
