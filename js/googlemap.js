13a9279063f27328000349a20371a88592ea9e3d
José Rasmussen <j.l.rasmussengarcia@gmail.com>
2017-02-02 13:34:48 +0100

    Google map addon

--- js/googlemap.js
+++ js/googlemap.js
@@ -2,10 +2,18 @@ function initMap() {
     var uluru = { lat: 51.917302, lng: 4.495713 };
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 15,
-        center: uluru
+        center: uluru,
+        mapTypeID: 'roadmap'
     });
+
+
+
     var marker = new google.maps.Marker({
         position: uluru,
         map: map
     });
 }
+
+//add an info window. look for it at
+//infowindows at developers.google.com
+
